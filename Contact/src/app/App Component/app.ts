45d74269af6dcfe16fd49b2df7977958cc2f2a99
Farhan Khan <khanjanfarhan2@gmail.com>
2025-08-06 22:05:06 +0500

    Development | #1

--- Contact/src/app/App Component/app.ts
+++ Contact/src/app/App Component/app.ts
@@ -1,7 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Post } from '../contact form/post';
-import { HttpService } from '../Services/http.service';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
