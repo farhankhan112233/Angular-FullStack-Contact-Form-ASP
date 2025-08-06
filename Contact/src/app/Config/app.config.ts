476b8d21baeb4f32b690d2202003513909257faf
Farhan Khan <khanjanfarhan2@gmail.com>
2025-08-06 13:33:33 +0500

    Code Align | #1

--- Contact/src/app/Config/app.config.ts
+++ Contact/src/app/Config/app.config.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { routes } from '../Routes/app.routes';
 import {
   provideClientHydration,
   withEventReplay,
