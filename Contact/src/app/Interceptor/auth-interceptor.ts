1e97db88a03436a22099a3c3e3cf73d4f260c97e
Farhan Khan <khanjanfarhan2@gmail.com>
2025-08-06 21:54:22 +0500

    JWT Config | #1

---
+++ Contact/src/app/Interceptor/auth-interceptor.ts
@@ -0,0 +1,42 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  private router = inject(Router);
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = sessionStorage.getItem('Token');
+    let authReq = req;
+
+    if (token) {
+      authReq = req.clone({
+        headers: req.headers.set('Authorization', token),
+      });
+      console.log('Hello from interceptor', authReq.headers);
+    }
+
+    return next.handle(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          sessionStorage.removeItem('Token');
+          this.router.navigate(['']);
+          alert('Token Expired');
+        }
+        return throwError(() => error);
+      })
+    );
+  }
+}
